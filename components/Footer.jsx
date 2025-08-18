4abac717a6481c6278636e30302a7263e0bd3782
Manmohan2016 <63162687+Shaileshsainee123@users.noreply.github.com>
2025-08-18 15:45:15 +0530

    Fix the issue  of linking in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Instagram,  Linkedin ,Github} from 'lucide-react'
+import Link from 'next/link';
 
 const Footer = () => {
     const getCurrentYear =  new Date().getFullYear();
@@ -8,13 +9,19 @@ const Footer = () => {
     <div className='flex flex-col items-center gap-4'>
      <div className='flex gap-4 '>
         <div className='bg-white rounded-full p-3 cursor-pointer hover:shadow-lg'>
-      <Instagram href='https://www.instagram.com/shaileshsainee/' target='_blank'  className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30}  color='#00d2b5'/>
+          <Link href='https://www.instagram.com/shaileshsainee/' target='_blank'>
+            <Instagram className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30} color='#00d2b5'/>
+          </Link>
         </div>
         <div className='bg-white rounded-full p-3 cursor-pointer hover:shadow-lg'>
-      <Linkedin href='https://www.linkedin.com/in/shailesh-sainee-63a7101a3/' target='_blank' className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30} color='#00d2b5'/>
+          <Link href='https://www.linkedin.com/in/shailesh-sainee-63a7101a3/' target='_blank'>
+            <Linkedin className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30} color='#00d2b5'/>
+          </Link>
         </div>
         <div className='bg-white rounded-full p-3 cursor-pointer hover:shadow-lg'>
-      <Github href='https://github.com/Shaileshsainee123' target='_blank' className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30}  color='#00d2b5'/>
+          <Link href='https://github.com/Shaileshsainee123' target='_blank'>
+            <Github className='transition ease-in-out delay-150 duration-300 hover:scale-110' size={30} color='#00d2b5'/>
+          </Link>
         </div>
      </div>
      <h2 className='text-white'>© {getCurrentYear}. All Rights Reserved. Designed by <a className='font-bold cursor-pointer underline' href='#home'>Shailesh</a></h2>
